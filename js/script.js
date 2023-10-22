90f22e123954b1307e2e3c990835f1f24acc6812
MyNameIsArando <tylercrowhurst123@gmail.com>
2023-10-22 13:17:01 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-document.getElementById('volume').volume = 0.04; /* Music Volume */
+var button = document.getElementById('bgvid');
+    document.getElementById('mute').onclick = function (){
+
+    if (video.muted === false) {    
+       video.muted = true;
+    }
+
+    else {
+        video.muted = false;
+    }
+
+    };
+    document.getElementById('bgvid').volume = 0.05; /* Music Volume */
